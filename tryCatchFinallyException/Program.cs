465573c04d407634c008094ce9404ca40d503ac8
Сергей Будников < @LAPTOP-H6G5MQOH>
2023-12-04 17:38:15 +0300

    Добавьте файлы проекта.

---
+++ tryCatchFinallyException/Program.cs
@@ -0,0 +1,20 @@
+﻿try
+{
+    try
+    {
+        throw new NotImplementedException();
+    }
+    catch
+    {
+        throw new NullReferenceException();
+    }
+    finally
+    {
+        throw new DivideByZeroException();
+    }
+}
+catch (Exception e)
+{
+    //Attempted to divide by zero.
+    Console.WriteLine(e.Message.ToString());
+}
